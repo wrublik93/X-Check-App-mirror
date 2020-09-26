b78de73a0d6a01e3250c4202e1cbf995de8f6088
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-09-26 13:11:33 +0300

    Implement project entities

--- src/types/entities.d.ts
+++ src/types/entities.d.ts
@@ -14,7 +14,7 @@ export interface User {
 export interface Course {
   id: number;
   name: string;
-  fullname: string;
+  fullName: string;
   description: string;
   startDate: Date;
   endDate: Date;
@@ -24,12 +24,104 @@ export interface Course {
 export interface Role {
   id: number;
   name: string;
-  rights: Rigths[];
+  rights: Rights[];
 }
 
-export interface Rigths {
+export interface Rights {
   read: boolean;
   create: boolean;
   delete: boolean;
   change: boolean;
+}
+
+export interface Session {
+  id: number;
+  status: number;
+  taskId: number;
+  coefficient: number;
+  startDate: Date;
+  endDate: Date;
+  discardMinScore: boolean;
+  minReviewAmount: number;
+  desiredReviewersAmount: number;
+  attendees: Attendees[];
+}
+
+export interface Attendees {
+  id: number;
+}
+
+export interface SessionStatus {
+  id: number;
+  name: string;
+}
+
+export interface Task {
+  id: number;
+  name: string;
+  description: string;
+  descriptionURL: string;
+  createdDate: Date;
+  updatedDate: Date;
+  authorId: number;
+  status: TaskStatus[];
+  categoryTask: CategoryTask[];
+  criterions: Criterions[];
+}
+
+export interface TaskStatus {
+  id: number;
+  name: string;
+}
+
+export interface CategoryTask {
+  id: number;
+  name: string;
+}
+
+export interface Criterions {
+  id: number;
+  minScore: number;
+  maxScore: number;
+  categoryTask: number;
+  title: string;
+  description: string;
+}
+
+export interface ReviewRequest {
+  id: number;
+  sessionId: number;
+  authorId: number;
+  authorGithubId: string;
+  task: number;
+  status: number;
+  selfGrade: SelfGrade[];
+}
+
+export interface SelfGrade {
+  id: number;
+}
+
+export interface ReviewRequestStatus {
+  id: number;
+  name: string;
+}
+
+export interface Review {
+  id: number;
+  requestId: number;
+  userId: number;
+  authorGithubId: string;
+  reviewStatus: number;
+  grade: GradeReview[];
+  isVisibleContactInfo: boolean;
+}
+
+export interface GradeReview {
+  id: number;
+}
+
+export interface ReviewStatus {
+  id: number;
+  name: string;
 }
