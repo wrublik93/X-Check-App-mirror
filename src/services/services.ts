3440e25457fbe63d5625d7f8e28d4418b49686bf
Alexey Koren <johnsmitlpf@gmail.com>
2020-09-27 02:10:55 +0300

    Fix getUser request

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,6 +1,7 @@
+import { User } from '@/types/entities';
 import { GetUserArgs } from '@/types/services';
 
-export const getUser = async ({ userId }: GetUserArgs): Promise<Response> => {
+export const getUser = async ({ userId }: GetUserArgs): Promise<User> => {
   const url = 'https://x-check-app-server-team25.herokuapp.com/user';
 
   const rawResponse = await fetch(`${url}?id=${userId}`, {
@@ -11,9 +12,7 @@ export const getUser = async ({ userId }: GetUserArgs): Promise<Response> => {
     },
   });
 
-  // eslint-disable-next-line no-console
-  console.log(await rawResponse.json());
-  return rawResponse;
+  return rawResponse.json() as Promise<User>;
 };
 
 export const test = (): void => {};
