7c832f7a526fdf7038488a446117c1a358cbea48
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-09-29 13:35:40 +0300

    feat: implement requests for user, role, task, course

--- src/services/services.ts
+++ src/services/services.ts
@@ -1,11 +1,79 @@
-import { User } from '@/types/entities';
-import { GetUserArgs } from '@/types/services';
+import {
+  Course, User, Task, TaskStatus, TaskCategories, Role,
+} from '@/types/entities';
+import {
+  LoginUserArgs,
+  CreateUserArgs,
+  EmailUser,
+  GetCourseArgs,
+  NameCourse,
+  CreateCourseArgs,
+  GetTaskArgs,
+  NameTask,
+  CreateTaskArgs,
+  GetRoleArgs,
+} from '@/types/services';
 
-export const getUser = async ({ userId }: GetUserArgs): Promise<User> => {
-  const url = 'https://x-check-app-server-team25.herokuapp.com/user';
+/* url*/
+const url = 'https://x-check-app-server-team25.herokuapp.com/';
 
-  const rawResponse = await fetch(`${url}?id=${userId}`, {
+/* log in yes or no*/
+export const logInUser = async ({ email, password }: LoginUserArgs): Promise<boolean> => {
+  const entity = 'users';
+  const rawResponse = await fetch(`${url}${entity}?email=${email}&&password=${password}`, {
     method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = (await rawResponse.json()) as [];
+  return !!data.length;
+};
+
+export const getUser = async ({ email, password }: LoginUserArgs): Promise<User> => {
+  const entity = 'users';
+  const rawResponse = await fetch(`${url}${entity}?email=${email}&&password=${password}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<User>;
+};
+
+export const getUsers = async (): Promise<User> => {
+  const entity = 'users';
+  const rawResponse = await fetch(`${url}${entity}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<User>;
+};
+
+/* check email's registration*/
+export const checkEmail = async ({ email }: EmailUser): Promise<boolean> => {
+  const entity = 'users';
+  const rawResponse = await fetch(`${url}${entity}?email=${email!}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = (await rawResponse.json()) as [];
+  return !!data.length;
+};
+
+/* Delete user by id*/
+export const deleteUser = async ({ id }: EmailUser): Promise<User> => {
+  const entity = 'users';
+  const rawResponse = await fetch(`${url}${entity}/${id!}`, {
+    method: 'DELETE',
     headers: {
       Accept: 'application/json',
       'Content-Type': 'application/json',
@@ -15,4 +83,284 @@ export const getUser = async ({ userId }: GetUserArgs): Promise<User> => {
   return rawResponse.json() as Promise<User>;
 };
 
-export const test = (): void => {};
+/* create user*/
+export const createUser = async ({
+  firstName,
+  lastName,
+  email,
+  password,
+}: CreateUserArgs): Promise<User | boolean> => {
+  const registered = await checkEmail({ email });
+  const entity = 'users';
+  if (!registered) {
+    const rawResponse = await fetch(`${url}${entity}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        firstName,
+        lastName,
+        email,
+        password,
+      }),
+    });
+    return rawResponse.json() as Promise<User>;
+  }
+  return false;
+};
+
+/** Get roles */
+export const getRoles = async (): Promise<Role> => {
+  const entity = 'roles';
+  const rawResponse = await fetch(`${url}${entity}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<Role>;
+};
+
+/** Get role by id */
+export const getRoleById = async ({ id }: GetRoleArgs): Promise<Role> => {
+  const entity = 'roles';
+  const rawResponse = await fetch(`${url}${entity}/${id}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<Role>;
+};
+
+/** Get course by id*/
+export const getCourseId = async ({ id }: GetCourseArgs): Promise<Course> => {
+  const entity = 'courses';
+  const rawResponse = await fetch(`${url}${entity}/${id!}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<Course>;
+};
+
+/** Get course by name */
+export const getCourseName = async ({ name }: GetCourseArgs): Promise<Course> => {
+  const entity = 'courses';
+  const rawResponse = await fetch(`${url}${entity}?name=${name!}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<Course>;
+};
+
+/** Get courses */
+export const getCourses = async (): Promise<Course> => {
+  const entity = 'courses';
+  const rawResponse = await fetch(`${url}${entity}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<Course>;
+};
+
+/** Check course */
+export const checkCourse = async ({ name }: NameCourse): Promise<boolean> => {
+  const entity = 'courses';
+  const rawResponse = await fetch(`${url}${entity}?name=${name!}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = (await rawResponse.json()) as [];
+  return !!data.length;
+};
+
+/** Create course */
+export const createCourse = async ({
+  name,
+  fullName,
+  description,
+  startDate,
+  endDate,
+  completed,
+}: CreateCourseArgs): Promise<Course | boolean> => {
+  const registered = await checkCourse({ name });
+  const entity = 'courses';
+  if (!registered) {
+    const rawResponse = await fetch(`${url}${entity}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        fullName,
+        description,
+        startDate,
+        endDate,
+        completed,
+      }),
+    });
+    return rawResponse.json() as Promise<Course>;
+  }
+  return false;
+};
+
+/* Delete user by id*/
+export const deleteCourse = async ({ id }: GetCourseArgs): Promise<User> => {
+  const entity = 'courses';
+  const rawResponse = await fetch(`${url}${entity}/${id!}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return rawResponse.json() as Promise<User>;
+};
+
+/** Get Task by id */
+export const getTaskId = async ({ id }: GetTaskArgs): Promise<Task> => {
+  const entity = 'tasks';
+  const rawResponse = await fetch(`${url}${entity}/${id!}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<Task>;
+};
+
+/** Get task by name */
+export const getTaskName = async ({ name }: GetTaskArgs): Promise<Task> => {
+  const entity = 'tasks';
+  const rawResponse = await fetch(`${url}${entity}?name=${name!}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<Task>;
+};
+
+/** Get tasks */
+export const getTasks = async (): Promise<Task> => {
+  const entity = 'tasks';
+  const rawResponse = await fetch(`${url}${entity}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<Task>;
+};
+
+/** Check task */
+export const checkTask = async ({ name }: NameTask): Promise<boolean> => {
+  const entity = 'tasks';
+  const rawResponse = await fetch(`${url}${entity}?name=${name}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  const data = (await rawResponse.json()) as [];
+  return !!data.length;
+};
+
+/** Create task */
+export const createTask = async ({
+  name,
+  description,
+  descriptionURL,
+  createdDate,
+  updatedDate,
+  authorId,
+  taskStatusId,
+  taskCategoryId,
+}: CreateTaskArgs): Promise<Task | boolean> => {
+  const registered = await checkTask({ name });
+  const entity = 'tasks';
+  if (!registered) {
+    const rawResponse = await fetch(`${url}${entity}`, {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name,
+        description,
+        descriptionURL,
+        createdDate,
+        updatedDate,
+        authorId,
+        taskStatusId,
+        taskCategoryId,
+      }),
+    });
+    return rawResponse.json() as Promise<Task>;
+  }
+  return false;
+};
+
+/* Delete task by id*/
+export const deleteTask = async ({ id }: GetTaskArgs): Promise<Task> => {
+  const entity = 'tasks';
+  const rawResponse = await fetch(`${url}${entity}/${id!}`, {
+    method: 'DELETE',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+
+  return rawResponse.json() as Promise<Task>;
+};
+
+/** Get task statuses */
+export const getTaskStatuses = async (): Promise<TaskStatus> => {
+  const entity = 'taskStatuses';
+  const rawResponse = await fetch(`${url}${entity}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<TaskStatus>;
+};
+
+/** Get task categories */
+export const getTaskCategories = async (): Promise<TaskCategories> => {
+  const entity = 'taskCategories';
+  const rawResponse = await fetch(`${url}${entity}`, {
+    method: 'GET',
+    headers: {
+      Accept: 'application/json',
+      'Content-Type': 'application/json',
+    },
+  });
+  return rawResponse.json() as Promise<TaskCategories>;
+};
--- src/types/services.d.ts
+++ src/types/services.d.ts
@@ -1,3 +1,66 @@
-export interface GetUserArgs {
-  userId: number;
+export interface LoginUserArgs {
+  email: string;
+  password: string;
+}
+
+export interface EmailUser {
+  id?: number;
+  email?: string;
+}
+
+export interface CreateUserArgs {
+  token?: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  githubId?: string;
+  githubToken?: string;
+  currentCourseIds?: number[];
+  roleIds?: number[];
+}
+
+export interface GetCourseArgs {
+  id?: number;
+  name?: string;
+}
+
+export interface NameCourse {
+  name?: string;
+  fullName?: string;
+}
+
+export interface CreateCourseArgs {
+  name: string;
+  fullName: string;
+  description: string;
+  startDate: Date;
+  endDate: Date;
+  completed: boolean;
+}
+
+export interface GetTaskArgs {
+  id?: number;
+  name?: string;
+}
+
+export interface NameTask {
+  name: string;
+}
+
+export interface CreateTaskArgs {
+  name: string;
+  description: string;
+  descriptionURL: string;
+  createdDate: Date;
+  updatedDate: Date;
+  authorId: number;
+  taskStatusId: number;
+  taskCategoryId: number;
+  criterionsCategoriesOrder?: number[];
+  criterionsIds?: number[];
+}
+
+export interface GetRoleArgs {
+  id: number;
 }
