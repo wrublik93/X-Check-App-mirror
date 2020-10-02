a167d4bb5926c0fd9efac8f295965c0a5913a716
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-03 01:59:51 +0300

    feat: add interfaces for component Forms

--- src/types/props.d.ts
+++ src/types/props.d.ts
@@ -2,3 +2,37 @@ export interface HeaderDataProps {
   name: string;
   link: string;
 }
+
+interface RulesInputProps {
+  required: boolean;
+  message: string;
+}
+
+interface RadioInputProps {
+  name: string;
+  value: string;
+}
+
+interface RulesRadioInputProps {
+  required: boolean;
+  message: string;
+}
+
+export interface FormInputProps {
+  label: string;
+  name: string;
+  placeholder: string;
+  rules: RulesInputProps;
+}
+
+export interface FormRadioGroupProps {
+  name: string;
+  label: string;
+  rules: RulesInputProps;
+  radioButtonInput: RadioInputProps[];
+}
+
+export interface FormInfoProps {
+  nameForm: string;
+  nameButton: string;
+}
