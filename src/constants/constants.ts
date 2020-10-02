fbd4fad4c6601e3812457f216d35f6b7e5938e8c
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-03 01:58:01 +0300

    feat: add constants for log in and sign up

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,4 +1,6 @@
-import { HeaderDataProps } from '@/types/props';
+import {
+  FormInputProps, HeaderDataProps, FormInfoProps, FormRadioGroupProps,
+} from '@/types/props';
 
 export const routeNamesMap = {
   '/': 'Registration',
@@ -11,9 +13,127 @@ export const routeNamesMap = {
 };
 
 export const headerLinks: HeaderDataProps[] = [
-  { name: 'Home', link: '/home' },
-  { name: 'Tasks', link: '/tasks' },
-  { name: 'Review Requests', link: '/reviewRequests' },
-  { name: 'Reviews', link: '/reviews' },
-  { name: 'About Us', link: '/aboutUs' },
+  {
+    name: 'Home',
+    link: '/home',
+  },
+  {
+    name: 'Tasks',
+    link: '/tasks',
+  },
+  {
+    name: 'Review Requests',
+    link: '/reviewRequests',
+  },
+  {
+    name: 'Reviews',
+    link: '/reviews',
+  },
+  {
+    name: 'About Us',
+    link: '/aboutUs',
+  },
+];
+
+export const logInInputs: FormInputProps[] = [
+  {
+    label: 'Email',
+    name: 'email',
+    placeholder: 'Input your email...',
+    rules: {
+      required: true,
+      message: 'Please input your email!',
+    },
+  },
+  {
+    label: 'Password',
+    name: 'password',
+    placeholder: 'Input your password...',
+    rules: {
+      required: true,
+      message: 'Please input your password!',
+    },
+  },
+];
+
+export const signUpInputs: FormInputProps[] = [
+  {
+    label: 'Email',
+    name: 'email',
+    placeholder: 'Input your email...',
+    rules: {
+      required: true,
+      message: 'Please input your email!',
+    },
+  },
+  {
+    label: 'Password',
+    name: 'password',
+    placeholder: 'Input your password...',
+    rules: {
+      required: true,
+      message: 'Please input your password!',
+    },
+  },
+  {
+    label: 'First name',
+    name: 'firstName',
+    placeholder: 'Input your first name...',
+    rules: {
+      required: true,
+      message: 'Please input your first name!',
+    },
+  },
+  {
+    label: 'Last name',
+    name: 'lastName',
+    placeholder: 'Input your last name...',
+    rules: {
+      required: true,
+      message: 'Please input your last name!',
+    },
+  },
+];
+
+export const logInInfo: FormInfoProps = {
+  nameForm: 'logIn',
+  nameButton: 'Log In',
+};
+
+export const signUpInfo: FormInfoProps = {
+  nameForm: 'signUp',
+  nameButton: 'Sign Up',
+};
+
+export const signUpRadios: FormRadioGroupProps[] = [
+  {
+    name: 'roles',
+    label: 'Roles',
+    rules: {
+      required: true,
+      message: 'Choose your role!',
+    },
+    radioButtonInput: [
+      {
+        name: 'Admin',
+        value: 'admin',
+      },
+      {
+        name: 'Trainer',
+        value: 'trainer',
+      },
+      {
+        name: 'Mentor',
+        value: 'mentor',
+      },
+      {
+        name: 'Activist',
+        value: 'activist',
+      },
+      {
+        name: 'Student',
+        value: 'student',
+      },
+    ],
+  },
 ];
