1f6442c6113a82a6b96071a1799c4f9c0bbf3df2
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-03 23:40:09 +0300

    feat: add actions and reducers for roles

--- src/store/actions/roles.ts
+++ src/store/actions/roles.ts
@@ -1,4 +1,4 @@
-/* import { rolesSlice } from '@/store/reducers/roles'; */
+import { rolesSlice } from '@/store/reducers/roles';
 
 // eslint-disable-next-line import/prefer-default-export
-/* export const {  } = rolesSlice.actions; */
+export const { getAllRoles } = rolesSlice.actions;
