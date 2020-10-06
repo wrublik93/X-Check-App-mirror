d477212adeb2aa3c9b77c704f8b54e63793117a2
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-07 00:12:55 +0300

    feat: add subscribe for store

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,5 +1,6 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 
+import alertsSliceReducer from '@/store/reducers/alerts';
 import coursesSliceReducer from '@/store/reducers/courses';
 import modalsSliceReducer from '@/store/reducers/modals';
 import reviewsSliceReducer from '@/store/reducers/reviews';
@@ -16,10 +17,19 @@ const rootReducer = combineReducers({
   tasks: tasksSliceReducer,
   reviews: reviewsSliceReducer,
   modals: modalsSliceReducer,
+  alerts: alertsSliceReducer,
 });
 
 export const store = configureStore({
   reducer: rootReducer,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  preloadedState: localStorage['x-check-app-redux']
+    ? JSON.parse(localStorage['x-check-app-redux'])
+    : {},
+});
+
+store.subscribe(() => {
+  localStorage['x-check-app-redux'] = JSON.stringify(store.getState());
 });
 
 export type RootState = ReturnType<typeof store.getState>;
