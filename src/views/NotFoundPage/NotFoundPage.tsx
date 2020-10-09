e905b0ed4d9a112e3dcdcffba77e13d6c149b1c2
Ivan Vrubleuski <wrublik.93@mail.ru>
2020-10-09 08:55:46 +0300

    feat: implement not found page

--- src/views/NotFoundPage/NotFoundPage.tsx
+++ src/views/NotFoundPage/NotFoundPage.tsx
@@ -1,9 +1,14 @@
+import Result from 'antd/lib/result';
 import React from 'react';
 
 import styles from '@/views/NotFoundPage/NotFoundPage.scss';
 
 const NotFoundPage = (): JSX.Element => (
-  <div className={styles['not-found-page']}>Not found page</div>
+  <div className={styles['review-requests']}>
+    <h1>404</h1>
+    <Result status="404" title="Not found page!" />
+    ,
+  </div>
 );
 
 export default NotFoundPage;
